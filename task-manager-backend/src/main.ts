9a3a46b4ae6340a714cec25a04aa95f38aa85af0
Harshpandey2165 <87353929+Harshpandey2165@users.noreply.github.com>
2025-04-07 18:20:09 +0530

    Update main.ts

--- task-manager-backend/src/main.ts
+++ task-manager-backend/src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:4200', 'https://task-app-ten-delta.vercel.app'],
+    origin: ['https://task-app-ten-delta.vercel.app','http://localhost:4200'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization', 'Origin', 'X-Requested-With', 'Accept'],
