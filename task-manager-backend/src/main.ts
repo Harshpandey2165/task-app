b1117a55120a02981c319a60505c6439b50b4a31
Harshpandey2165 <harshpandey00009@gmail.com>
2025-04-07 17:13:51 +0530

    Update CORS configuration to allow Vercel frontend

--- task-manager-backend/src/main.ts
+++ task-manager-backend/src/main.ts
@@ -5,9 +5,11 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:4200', // Your frontend application URL
+    origin: ['http://localhost:4200', 'https://task-app-tdx9.vercel.app'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    exposedHeaders: ['Authorization']
   });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
